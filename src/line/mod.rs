ecbc927d9d1ba1ea904a966d5808d177998fe289
Lucas Timmins <timmins.s.lucas@gmail.com>
2018-07-28 13:42:53 +0800

    Add bresenham lines

--- src/line/mod.rs
+++ src/line/mod.rs
@@ -1,3 +1,4 @@
+mod bresenham;
 use Draw;
 
 pub struct Line {
@@ -20,22 +21,36 @@ impl Draw for Line {
     fn draw(&self, canvas: &mut[u8], canvas_size: (usize, usize)) {
         if self.pt1.0 == self.pt2.0 {
             // Straight vertical line
-            for i in 0..(self.pt2.1 - self.pt1.1) + 1 {
+            let (pt1, pt2) = if self.pt1.1 > self.pt2.1 {
+                (self.pt2, self.pt1)
+            } else {
+                (self.pt1, self.pt2)
+            };
+            for i in pt1.1..pt2.1 + 1 {
                 for c in 0..4 {
-                    canvas[4 * (canvas_size.0 * i + self.pt1.0) + c] = self.color[c];
+                    canvas[4 * (canvas_size.0 * i + pt1.0) + c] = self.color[c];
                 }
             }
         } else if self.pt1.1 == self.pt2.1 {
             // Straight horizontal line 
-            for i in 0..(self.pt2.0 - self.pt1.0) + 1 {
+            let (pt1, pt2) = if self.pt1.0 > self.pt2.0 {
+                (self.pt2, self.pt1)
+            } else {
+                (self.pt1, self.pt2)
+            };
+            for i in pt1.0..pt2.0 + 1 {
                 for c in 0..4 {
-                    canvas[4 * (canvas_size.0 * self.pt1.1 + self.pt1.0 + i) + c] = self.color[c];
+                    canvas[4 * (canvas_size.0 * pt1.1 + i) + c] = self.color[c];
                 }
             }
         } else {
             // Angled line
-            let gradient = (self.pt2.1 - self.pt1.1) as f32 / (self.pt2.0 - self.pt1.0) as f32;  
-            println!("gradient = {}", gradient);
+            for pt in bresenham::BresenhamLine::new(self.pt1, self.pt2).pts {
+                println!("{}, {}", pt.0, pt.1);
+                for c in 0..4 {
+                    canvas[4 * (canvas_size.0 * pt.1 + pt.0) + c] = self.color[c];
+                }
+            }
         }
     }
 }
